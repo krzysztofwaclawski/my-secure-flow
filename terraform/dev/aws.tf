53ca03bd74ba1d27c178be48b03f083fa1ad48ee
arglo11 <124684301+arglo11@users.noreply.github.com>
2023-02-21 14:17:08 +0200

    Update aws.tf

--- terraform/dev/aws.tf
+++ terraform/dev/aws.tf
@@ -2,29 +2,95 @@ provider "aws" {
   region = "us-west-2"
 }
 
+locals {
+  name   = "demo1"
+  region = "eu-west-1"
+
+  tags = {
+    Env    = local.name
+  }
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
+  version = "3.19.0"
 
-  name = "k8s-vpc"
+  name = local.name
   cidr = "10.0.0.0/16"
-  subnets = [
-    {
-      cidr = "10.0.0.0/24"
-      azs  = ["us-west-2a"]
-      type = "private"
-    },
-    {
-      cidr = "10.0.1.0/24"
-      azs  = ["us-west-2b"]
-      type = "private"
-    },
-    {
-      cidr = "10.0.2.0/24"
-      azs  = ["us-west-2c"]
-      type = "private"
-    },
-  ]
+
+  azs                 = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets      = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  database_subnets    = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+
+  intra_subnets       = ["10.0.51.0/24", "10.0.52.0/24", "10.0.53.0/24"]
+
+  private_subnet_names = ["Private Subnet One", "Private Subnet Two"]
+  # public_subnet_names omitted to show default name generation for all three subnets
+  database_subnet_names    = ["DB Subnet One"]
+
+  intra_subnet_names       = []
+
+  create_database_subnet_group = false
+
+  manage_default_network_acl = true
+  default_network_acl_tags   = { Name = "${local.name}-default" }
+
+  manage_default_route_table = true
+  default_route_table_tags   = { Name = "${local.name}-default" }
+
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "${local.name}-default" }
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = local.tags
+}
+  
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+
+data "aws_iam_policy_document" "generic_endpoint_policy" {
+  statement {
+    effect    = "Deny"
+    actions   = ["*"]
+    resources = ["*"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:SourceVpc"
+
+      values = [module.vpc.vpc_id]
+    }
+  }
+}
+
+resource "aws_security_group" "vpc_tls" {
+  name_prefix = "${local.name}-vpc_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  tags = local.tags
 }
 
 module "eks" {
