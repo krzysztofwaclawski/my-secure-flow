ce1124e1ed9d080632972c089b9566bf5bc7cf20
arglo11 <124684301+arglo11@users.noreply.github.com>
2023-02-21 14:40:09 +0200

    Update aws.tf

--- terraform/dev/aws.tf
+++ terraform/dev/aws.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
 
 locals {
   name   = "demo1"
-  region = "us-west-1"
+  region = "us-east-1"
 
   tags = {
     Env    = local.name
@@ -18,7 +18,7 @@ module "vpc" {
   name = local.name
   cidr = "10.0.0.0/16"
 
-  azs                 = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  azs                 = ["${local.region}a", "${local.region}b", "${local.region}c","${local.region}d","${local.region}e"]
   private_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets      = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
   database_subnets    = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
